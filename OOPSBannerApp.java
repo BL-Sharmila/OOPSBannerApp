169e642d78dcae2e791ce37c4cc6402171747af6
BL-Sharmila <sharmila.gunasekar@bridgelabz.com>
2026-01-05 15:50:56 +0530

    [Sharmila]-Feat: UC7: Store character pattern in a class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,38 +1,40 @@
 /**
- * UC6: Refactor Banner Logic into Functions
+ * UC7: Store Character Pattern in a Class
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = buildBanner();
+        CharacterPattern O = new CharacterPattern('O', getO());
+        CharacterPattern P = new CharacterPattern('P', getP());
+        CharacterPattern S = new CharacterPattern('S', getS());
 
-        for (String line : banner) {
+        CharacterPattern[] word = {O, O, P, S};
+
+        for (int i = 0; i < 7; i++) {
+            StringBuilder line = new StringBuilder();
+            for (CharacterPattern cp : word) {
+                line.append(cp.getPattern()[i]).append(" ");
+            }
             System.out.println(line);
         }
     }
 
-    static String[] buildBanner() {
-        return new String[]{
-                String.join(" ", getO()[0], getO()[0], getP()[0], getS()[0]),
-                String.join(" ", getO()[1], getO()[1], getP()[1], getS()[1]),
-                String.join(" ", getO()[2], getO()[2], getP()[2], getS()[2]),
-                String.join(" ", getO()[3], getO()[3], getP()[3], getS()[3]),
-                String.join(" ", getO()[4], getO()[4], getP()[4], getS()[4]),
-                String.join(" ", getO()[5], getO()[5], getP()[5], getS()[5]),
-                String.join(" ", getO()[6], getO()[6], getP()[6], getS()[6])
-        };
+    static class CharacterPattern {
+        private final char character;
+        private final String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
     }
 
-    static String[] getO() {
-        return new String[]{" ***** ", " *   * ", " *   * ", " *   * ", " *   * ", " *   * ", " ***** "};
-    }
-
-    static String[] getP() {
-        return new String[]{" ****  ", " *   * ", " *   * ", " ****  ", " *     ", " *     ", " *     "};
-    }
-
-    static String[] getS() {
-        return new String[]{" ***** ", " *     ", " *     ", " ****  ", "     * ", "     * ", " ***** "};
-    }
+    static String[] getO() { return new String[]{" ***** "," *   * "," *   * "," *   * "," *   * "," *   * "," ***** "}; }
+    static String[] getP() { return new String[]{" ****  "," *   * "," *   * "," ****  "," *     "," *     "," *     "}; }
+    static String[] getS() { return new String[]{" ***** "," *     "," *     "," ****  ","     * ","     * "," ***** "}; }
 }
